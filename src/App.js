a6f3353bba1180d7ec3dc80331f9b91c32c6340a
lucianoaraus <lucianoaraus@gmail.com>
2022-03-31 12:34:50 -0300

    css fixed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import Header from "./components/header/Header";
 import Headline from "./components/headline/Headline";
-import ItemListContainer from "./components/item-list-container/ItemListContainer"
+import ItemListContainer from "./components/item-list-container/ItemListContainer";
 import ItemSelector from "./components/item-selector/ItemSelector";
 
 import "./app.css";
 
-
-function App(){
-
-  const [state, setState] = useState(false)
+function App() {
+  const [state, setState] = useState(false);
 
   // == handleOpen
   const changeLayout = () => {
-    setState(!state)
-    
-  }
+    setState(!state);
+  };
 
   return (
-    
     <div className="app">
       <Header />
-      <Headline/>
-      <h3>CLICK ON THE <b>"SEARCH ICON"</b> TO SHOW AVAILABLE OPTIONS</h3>
-      {
-        state? <ItemListContainer callBack={() => changeLayout()}/> : <ItemSelector callBack={() => changeLayout()}/>
-      }
+      <Headline />
+      <h3>
+        CLICK ON THE <b>"SEARCH ICON"</b> TO SHOW AVAILABLE OPTIONS
+      </h3>
+      {state ? (
+        <ItemListContainer callBack={() => changeLayout()} />
+      ) : (
+        <ItemSelector callBack={() => changeLayout()} />
+      )}
     </div>
   );
 }
 
 export default App;
-
