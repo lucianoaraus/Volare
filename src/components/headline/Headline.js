218bbaa9c66a05e175cdad74de59dab80e78e8fc
lucianoaraus <lucianoaraus@gmail.com>
2022-04-18 16:31:38 -0300

    headline UX updated

--- src/components/headline/Headline.js
+++ src/components/headline/Headline.js
@@ -1,20 +1,24 @@
 import React from "react";
-import img from "../../assets/travel-girl.png"
+import img from "../../assets/travel-girl.png";
 
 import "../headline/headline.css";
 
-function Headline () {
+function Headline() {
   return (
     <div className="headline">
+      <img className="travel-girl" src={img} alt="travel-girl" />
       <div className="headline-text">
         <h3>Best Destinations around the world</h3>
-        <p className="headline-subtitle">Ready for your next Adventure? Travel with Volare</p>
-        <p className="headline-detail">Find the Ideal Lodging for your Trip. Compare Prices and Save with Volare! Are you looking for Hotel? Volare</p>
+        <p className="headline-subtitle">
+          Ready for your next Adventure? Travel with Volare
+        </p>
+        <p className="headline-detail">
+          Find the Ideal Lodging for your Trip. Compare Prices and Save with
+          Volare! Are you looking for Hotel? Volare
+        </p>
       </div>
-      <img className="travel-girl" src={img} alt="travel-girl"/>
     </div>
   );
 }
 
 export default Headline;
-
