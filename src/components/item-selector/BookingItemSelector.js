ce645002eba4414fdcb3ed6f0ea0ec785b33f053
lucianoaraus <lucianoaraus@gmail.com>
2022-04-21 20:36:32 -0300

    POST method wip

--- src/components/item-selector/BookingItemSelector.js
+++ src/components/item-selector/BookingItemSelector.js
@@ -25,15 +25,24 @@ function BookingItemSelector() {
   const { guestsQuantity } = useContext(BookingQueryContext);
 
   // WIP:
-  const [query, setQuery] = useState({
+  /* const [query, setQuery] = useState({
     queryDestination: "",
     queryCheckInDate: "",
     queryCheckOutDate: "",
     queryGuests: "",
-  });
+  }); */
+
+  /* const handleSubmit = () => {
+    setQuery({
+      queryDestination: "",
+      queryCheckInDate: "",
+      queryCheckOutDate: "",
+      queryGuests: "",
+    });
+  }; */
 
   const notGuestsEnought = () => {
-    // TODO: Arreglar esto
+    // TODO: arreglar esto
     0 < guestsQuantity && guestsQuantity < 5
       ? navigate("/booking/items")
       : alert(
@@ -41,87 +50,90 @@ function BookingItemSelector() {
         );
   };
 
-  const handleSubmit = () => {
-    setQuery({
-      queryDestination: "",
-      queryCheckInDate: "",
-      queryCheckOutDate: "",
-      queryGuests: "",
-    });
-  };
-
   console.log(
     `Destionation: ${destination}, Check-In: ${checkInDate}, Check-Out: ${checkOutDate}, Guests: ${guestsQuantity}`
   );
 
+  // TODO: Implementar el seteo de datos mediante un form (?)
+
   return (
     <div className="item-selector-container">
-      <form onSubmit={handleSubmit}>
-        <div className="options-selector">
-          <div className="destination-item" role="button">
-            <div className="left-selector">
-              <LocationOnIcon sx={{ fontSize: 32 }} />
-            </div>
-            <div className="mid-selector">
-              <h3>Destinations</h3>
-              <input
-                type="text"
-                id="destination"
-                placeholder="Where are you going?"
-                onChange={(event) => setDestination(event.target.value)}
-              ></input>
-            </div>
+      <div className="options-selector">
+        <div className="destination-item" role="button">
+          <div className="left-selector">
+            <LocationOnIcon sx={{ fontSize: 32 }} />
+          </div>
+          <div className="mid-selector">
+            <h3>Destinations</h3>
+            <input
+              type="text"
+              name="destination"
+              placeholder="Where are you going?"
+              onChange={(event) => setDestination(event.target.value)}
+            />
+          </div>
+          <button className="down-arrow">
+            <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
+          </button>
+        </div>
+
+        <div className="date-item" role="button">
+          <div className="left-selector">
+            <CalendarMonthIcon sx={{ fontSize: 32 }} />
+          </div>
+          <div className="mid-selector">
+            <h3>Check-in</h3>
+            <DatePickerSelector
+              onChange={(date) => setCheckInDate(date)}
+              type="text"
+              name="checkIn"
+            />
+          </div>
+          <button className="down-arrow">
+            <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
+          </button>
+        </div>
+
+        <div className="date-item" role="button">
+          <div className="left-selector">
+            <CalendarMonthIcon sx={{ fontSize: 32 }} />
+          </div>
+          <div className="mid-selector">
+            <h3>Check-out</h3>
+            <DatePickerSelector
+              onChange={(date) => setCheckOutDate(date)}
+              type="text"
+              name="checkOut"
+            />
+          </div>
+          <button className="down-arrow">
+            <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
+          </button>
+        </div>
+
+        <div className="guest-item" role="button">
+          <div className="left-selector">
+            <PersonOutlineIcon sx={{ fontSize: 32 }} />
+          </div>
+          <div className="mid-selector">
+            <MenuGuests type="number" name="guests" />
+          </div>
+          <div className="right-selector">
             <button className="down-arrow">
               <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
             </button>
-          </div>
 
-          <div className="date-item" role="button">
-            <div className="left-selector">
-              <CalendarMonthIcon sx={{ fontSize: 32 }} />
-            </div>
-            <div className="mid-selector">
-              <h3>Check-in</h3>
-              <DatePickerSelector onChange={(date) => setCheckInDate(date)} />
-            </div>
-            <button className="down-arrow">
-              <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
+            <button
+              className="button-4"
+              onClick={notGuestsEnought}
+              type="submit"
+            >
+              <SearchIcon sx={{ fontSize: 32 }} />
+              Search
             </button>
-          </div>
-
-          <div className="date-item" role="button">
-            <div className="left-selector">
-              <CalendarMonthIcon sx={{ fontSize: 32 }} />
-            </div>
-            <div className="mid-selector">
-              <h3>Check-out</h3>
-              <DatePickerSelector onChange={(date) => setCheckOutDate(date)} />
-            </div>
-            <button className="down-arrow">
-              <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
-            </button>
-          </div>
-
-          <div className="guest-item" role="button">
-            <div className="left-selector">
-              <PersonOutlineIcon sx={{ fontSize: 32 }} />
-            </div>
-            <div className="mid-selector">
-              <MenuGuests />
-            </div>
-            <div className="right-selector">
-              <button className="down-arrow">
-                <KeyboardArrowDownIcon sx={{ fontSize: 24 }} />
-              </button>
-
-              <button className="button-4" onClick={notGuestsEnought}>
-                <SearchIcon sx={{ fontSize: 32 }} />
-                Search
-              </button>
-            </div>
           </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 }
